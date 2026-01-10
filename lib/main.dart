9017572f7ec9575f74d319c44362845bcd95f6e8
albert-jd <albert-jd7470@users.noreply.github.com>
2026-01-10 09:43:05 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smec/screens/homepage.dart';
+import 'package:smec/widgets/navigation.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-home: Homepage(),
+home: BottomNavig(),
     );
   }
 }
