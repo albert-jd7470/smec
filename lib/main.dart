ca055a59b9db0ac0c2af2de8dd54266512a1a6b1
razeenkanely <razeenkanely@gmail.com>
2026-01-15 20:48:58 +0530

    razeen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smec/screens/homepage.dart';
+
 import 'package:smec/screens/splash_screen.dart';
 
 import 'package:smec/widgets/navigation.dart';
